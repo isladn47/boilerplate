1084a23da3fcdfabe3fe054469d2678133293d0d
island47 <island0318@gmail.com>
2020-11-19 14:35:54 +0900

    4444

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+const { modelNames } = require("mongoose");
+
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+};
--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
 const mongoose = require("mongoose");
+const config = require("./config/key");
+
+// application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://jiwook:yangjiuk47@cluster0.ilk5h.mongodb.net/cluster0?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!~~~");
+  res.send("Hello World!ddd~~~");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데디터 베이스에 넣어준다.
+
+  const user = new User(req, body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
